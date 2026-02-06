344aebf63589a00698605fe26ca1b146ecf0406c
amolsalekar-8 <amolsalekar0280@gmail.com>
2026-02-06 07:32:27 +0530

    Morris Pre Order

--- src/com/dsa/tree/BinaryTree/algorithm/MorrisPreOrder.java
+++ src/com/dsa/tree/BinaryTree/algorithm/MorrisPreOrder.java
@@ -16,8 +16,8 @@ import com.dsa.tree.BinaryTree.utility.TreeNode;
  *  	3.2 : if its not null the assign currentNodes -> left to prevNode
  *  	3.3 : got till prevNode -> right is not null and prevNode-> right is not equal to currentNode
  *  	3.4 : UPTO  prevNode -> right  becomes null till assign currentNode to prevNode-> right [ Transforming into chain ]
- * 		3.5 : if prevNode -> right become null then assign prevNode -> right to current node
- * 		3.6 : otherwise prevNode -> right is current node then change prevNode -> right to null -> process curr node -> currentNode -> right 
+ * 		3.5 : if prevNode -> right become null then assign prevNode -> right to current node and process curr node
+ * 		3.6 : otherwise prevNode -> right is current node then change prevNode -> right to null  -> currentNode -> right 
  * 
  * 
  * 
@@ -47,26 +47,35 @@ public class MorrisPreOrder {
 	}
 	
 	public static List<Integer> morrisPreOrder(TreeNode root){
-		List<Integer>  morrisPreorderList = new ArrayList();
-		
-		TreeNode currNode = root;
+		List<Integer> morrisPreorderList = new ArrayList<>();
+		// Set the root node as curr node
+		TreeNode currNode  = root;
 		while(currNode != null) {
-			
-			if(currNode.left == null ) {
+			/** -- If currNode left  is null then process currNode and go for right side   -- */
+			if(currNode.left == null) {
 				morrisPreorderList.add(currNode.data);
 				currNode = currNode.right;
 			}else {
+				/** -- assign  currNode -> left  to prevNode   -- */
 				TreeNode prevNode = currNode.left;
+				/** -- If prevNode -> right  is not null and its should not equal to current node    -- */
 				while(prevNode.right != null && prevNode.right != currNode) {
 					prevNode = prevNode.right;
 				}
-				if(prevNode.right == null ) {
-					prevNode.right = currNode;
+				/** -- If prevNode -> right is null then change that pointer to current node  -- */
+				if(prevNode.right == null) {
+					/*-- Process the node -- */
 					morrisPreorderList.add(currNode.data);
+					/** -- assign prevNode -> right  pointer ->  currNodes  -- */
+					prevNode.right = currNode;
+					/** -- change current node to  currNodes.left  */
 					currNode = currNode.left;
 				}else {
+					/** -- if there is prevNode -> right node present and its equal to current node then 
+					 * 	make prevNode -> right to null then process it and change the pointer
+					 *  -- */
 					prevNode.right = null;
-					currNode = currNode.right;
+					currNode = currNode.right;				
 				}
 			}
 		}
